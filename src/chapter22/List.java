3ec9fbf6a628b5d798eb1c20dbfc872f6dbb0645
MdJubair14 <bsse0614@iit.du.ac.bd>
2015-06-01 16:44:04 +0600

    list created done

--- src/chapter22/List.java
+++ src/chapter22/List.java
@@ -9,16 +9,92 @@ package chapter22;
  * 1:27:23 PM
  * chapter22
  */
-public class List {
+public class List<T> {
 	private ListNode<T> firstNode;
 	private ListNode<T> lastNode;
 	private String name;
 	
 	public List(){
-		
+		this("List");
 	}
 	
-	public List(String name, ListNode<T> next){
+	public List(String name){
+		this.name = name;
+		firstNode = lastNode = null;
+	}
+
+	public void insertAtFront(T insertName){
+		if( isEmpty() ){
+			firstNode = lastNode = new ListNode<T>(insertName);
+		}
+		else
+			firstNode = new ListNode<T>(insertName, firstNode);
+	}
+
+	public void insertAtBack(T insertName){
+		if( isEmpty() ){
+			firstNode = lastNode = new ListNode<T>(insertName);
+		}
+		else
+			lastNode = lastNode.nextNode = new ListNode<T>(insertName);
+	}
+	
+	public T removedFromFront() throws EmptyListException{
+		if( isEmpty() ) 
+			throw new EmptyListException(name);
+		T removedItem = firstNode.data;
 		
+		if(firstNode == lastNode){
+			firstNode = lastNode = null;
+		}
+		else
+			firstNode = firstNode.nextNode;
+		
+		return removedItem;
+	}
+	
+	public T removedFromBack() throws EmptyListException{
+		
+		if( isEmpty() ){
+			throw new EmptyListException();
+		}
+		
+		T removedItem = lastNode.data;
+		
+		if(firstNode == lastNode)
+			firstNode = lastNode = null;
+		else{
+			ListNode<T> current = firstNode;
+			
+			while(current.nextNode != lastNode){
+				current = current.nextNode;
+			}
+			
+			lastNode = current;
+			current.nextNode = null;
+		}
+		
+		return removedItem;
+	}
+	
+	private boolean isEmpty() {
+		return firstNode == null;
+	}
+	
+	public void print(){
+		if( isEmpty() ){
+			System.out.println(name + " is empty");
+			return;
+		}
+		
+		System.out.printf("This %s is: ", name);
+		ListNode<T> current = firstNode;
+		
+		while(current != null){
+			System.out.printf(" %s ", current.data);
+			current = current.nextNode;
+		}
+		
+		System.out.println("\n");
 	}
 }
